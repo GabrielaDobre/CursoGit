66a5ca2d1340412531a2d38490251cbda491a45b
Gabriela <gabriela.dobre1604@gmail.com>
2024-07-23 11:09:04 +0200

    primera prueba

---
+++ Titulares.java
@@ -0,0 +1,21 @@
+package practicasPrimerBloque;
+
+public class Titulares{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	Cuenta miCuenta=new Cuenta("Juan");
+	Cuenta miCuenta1=new Cuenta ("Pablo");
+
+	miCuenta.ingresar(5.2);
+	miCuenta.ingresar(5);
+	miCuenta.retirar(4.1);
+	
+	miCuenta.cantidadToString();
+
+	System.out.println("En la cuenta de: " + miCuenta.getTitular() + ", hay un total de: " + miCuenta.getCantidad()+ miCuenta.cantidadToString() );
+	}
+}
